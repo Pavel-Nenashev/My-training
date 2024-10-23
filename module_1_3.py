2330b0777e8d5d8b10aac3b4fddfb2d4d58dba21
Pavel-Nenashev <pavelnenashev81@gmail.com>
2024-10-23 17:29:14 +0500

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+# Домашняя работа по теме Динамичская типизация
+
+name = "Pavel"
+print("Name: ", name)
+age = 42
+print("Age: ", age)
+new_age = age + 1
+print("New_age: ", new_age)
+is_student = True
+print("Is Student: ", is_student)
