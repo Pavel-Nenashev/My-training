7429b6d4ec476f73f167dc0ed91bf723f2a3aa25
Pavel-Nenashev <pavelnenashev81@gmail.com>
2024-10-25 12:47:58 +0500

    Add files via upload

---
+++ 1.2.py
@@ -0,0 +1,5 @@
+course_id = "Python"
+quantity_homeworks = 12          # quantity  - количество (англ.)
+quantity_time = 1.5
+one_task_time =  quantity_time / quantity_homeworks
+print("Курс:",course_id,"всего задач:",quantity_homeworks,"затрачено часов:",quantity_time,"среднее время выполнения:",one_task_time,"часа")
