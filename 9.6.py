4d4eb39474c7143f8b9cbbd12eac02fcbef50353
Pavel-Nenashev <pavelnenashev81@gmail.com>
2025-01-15 21:04:01 +0500

    Update 9.6.py

--- 9.6.py
+++ 9.6.py
@@ -1,4 +1,5 @@
 def all_variants(text):
+    '''
     length = len(text)
     # Проходим по всем возможным начальным индексам
     for start in range(length):
@@ -6,6 +7,13 @@ def all_variants(text):
         for end in range(start + 1, length + 1):
             # Возвращаем подпоследовательность
             yield text[start:end]
+     ''' 
+      # Исправлено
+    n = len(text)                                  # Получаем длину исходной строки.
+    for length in range(1, n + 1):                 # Перебираем все возможные длины подстрок от 1 до полной длины строки.
+        for start in range(n - length + 1):        # Для каждой длины перебираем все возможные стартовые позиции подстроки.
+            substring = text[start:start + length] # Извлекаем подстроку заданной длины начиная с позиции `start`.
+            yield substring                        # Возвращаем подстроку с помощью оператора `yield`.       
 
 # Пример выполнения программы
 a = all_variants("abc")
